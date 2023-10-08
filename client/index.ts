1cd04b2f6521b15bfbda8741c639dc4f726991ff
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2023-10-09 00:38:06 +0530

    Relay Completed and Client Partial

--- client/index.ts
+++ client/index.ts
@@ -1,12 +1,27 @@
-import * as net from 'net';
+'use strict';
+import {
+    get_udp_socket
+} from './sockets';
 
+const get_peer_address = (port: number, relay_name: string, relay_port: number) => {
+    console.log(`Will open current connection from port: ${port}`);
+
+    const udp = get_udp_socket('0.0.0.0', port);
+    
+    for (let i = 0; i < 10; i++) {
+        udp.send(relay_name, relay_port, 'Hello to relay!');
+    }
+    
+    return new Promise((resolve, reject) => udp.onMessage((IP, PORT, message) => {
+        udp.closeSocket();
+        const peer_ip = message.split(':')[0];
+        const peer_port = parseInt(message.split(':')[1]);
+        setTimeout(() => resolve({IP: peer_ip, PORT: peer_port}), 1000);
+    }));
+};
 
 const port = parseInt(process.argv[2]);
-const relay_ip = process.argv[3];
+const relay_name = process.argv[3];
 const relay_port = parseInt(process.argv[4]);
-console.log(`Will open peer on port: ${port}`);
-
-let server_ip = null;
-let server_port = null;
-
-let client = new net.Socket();
+const peer_address = get_peer_address(port, relay_name, relay_port);
+console.log(peer_address);
--- relay/index.ts
+++ relay/index.ts
@@ -1,33 +1,43 @@
+'use strict';
 import {
-    get_tcp_socket, get_udp_socket
+    get_udp_socket
 } from './sockets';
 
+function searchStringInArray(str: string, strArray: string[]) {
+    for (var j = 0; j < strArray.length; j++) 
+        if (strArray[j].match(str))
+            return true;
+    return false;
+}
+
 const port = parseInt(process.argv[2]);
-console.log(`Will open relay on port: ${port}`);
+console.log(`Will open relay on UDP port: ${port}`);
+const udp = get_udp_socket('0.0.0.0', port);
 
-let database = []
-const tcp = get_tcp_socket('localhost', port);
+let database = [];
 
-tcp.onNewConnection((IP, PORT) => {
-    database.push([IP, PORT]);
+udp.onMessage((IP ,PORT, message) => {
+    const to_add = `${IP}:${PORT}`;
+
+    if (!searchStringInArray(to_add, database))
+        database.push(to_add);
+    else
+        return;
+
+    console.log(`Received message from ${database[-1]} with content: ${message}`);
     if (database.length != 2)
         return;
 
     for (let i = 0; i < database.length; i++) {
-        const send_IP = database[i][0];
-        const send_PORT = database[i][1];
+        const send_IP = database[i].split(':')[0];
+        const send_PORT = parseInt(database[i].split(':')[1]);
 
-        tcp.send(send_IP, send_PORT, `${database[1 - i][0]}:${database[1 - i][1]}`);
+        for (let j = 0; j < 5; ++j)
+            udp.send(send_IP, send_PORT, `${database[1 - i]}`);
     }
-
-    // clear the database
+    
+    // clean database
+    database.length = 0;
     database = [];
-});
-
-tcp.onMessage((IP, PORT, message) => {
-    if (message !== `Break the connecti0n`)
-        return;
-
-    console.log(`TCP: ${IP}:${PORT} -> ${message}`);
-    tcp.closeConnection(IP, PORT);
+    udp.closeSocket();
 });
