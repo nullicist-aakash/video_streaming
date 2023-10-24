9dfa9bc3f3315c0400d9f11c499f6443c741b5ff
Aakash Gupta <f20180887@pilani.bits-pilani.ac.in>
2023-10-24 18:18:09 +0530

    Got the code for TCP Checksum

--- peer/client.cpp
+++ peer/client.cpp
@@ -4,10 +4,47 @@
 #include <unistd.h>
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
+#include <netinet/udp.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
 #define BUFFER_SIZE 65536
+void compute_tcp_checksum(struct iphdr *pIph, unsigned short *ipPayload) {
+    register unsigned long sum = 0;
+    unsigned short tcpLen = ntohs(pIph->tot_len) - (pIph->ihl<<2) - 4;
+    struct tcphdr *tcphdrp = (struct tcphdr*)(ipPayload + 4);
+    //add the pseudo header 
+    //the source ip
+    sum += (pIph->saddr>>16)&0xFFFF;
+    sum += (pIph->saddr)&0xFFFF;
+    //the dest ip
+    sum += (pIph->daddr>>16)&0xFFFF;
+    sum += (pIph->daddr)&0xFFFF;
+    //protocol and reserved: 6
+    sum += htons(IPPROTO_TCP);
+    //the length
+    sum += htons(tcpLen);
+ 
+    //add the IP payload
+    //initialize checksum to 0
+    tcphdrp->check = 0;
+    while (tcpLen > 1) {
+        sum += * ipPayload++;
+        tcpLen -= 2;
+    }
+    //if any bytes left, pad the bytes and add
+    if(tcpLen > 0) {
+        //printf("+++++++++++padding, %dn", tcpLen);
+        sum += ((*ipPayload)&htons(0xFF00));
+    }
+      //Fold 32-bit sum to 16 bits: add carrier to result
+      while (sum>>16) {
+          sum = (sum & 0xffff) + (sum >> 16);
+      }
+      sum = ~sum;
+    //set computation result
+    tcphdrp->check = (unsigned short)sum;
+}
 
 int main() {
     int raw_socket;
@@ -33,23 +70,13 @@ int main() {
         struct iphdr* ip_header = (struct iphdr*)buffer;
         if (ip_header->protocol == IPPROTO_TCP) {
             struct tcphdr* tcp_header = (struct tcphdr*)(buffer + ip_header->ihl * 4);
-            int payload_offset = ip_header->ihl * 4 + tcp_header->doff * 4;
-            int payload_size = packet_size - payload_offset;
-
-            // Print source and destination addresses and ports
-            printf("Source IP: %s\n", inet_ntoa(source_addr.sin_addr));
-            printf("Source Port: %d\n", ntohs(tcp_header->source));
-            printf("Dest IP: %s\n", inet_ntoa(source_addr.sin_addr));
-            printf("Dest Port: %d\n", ntohs(tcp_header->dest));
-
-            // Print payload (if any)
-            if (payload_size > 0) {
-                printf("Payload:\n");
-                for (int i = payload_offset; i < packet_size; i++) {
-                    printf("%02X ", buffer[i]);
-                }
-                printf("\n");
+            if (ntohs(tcp_header->dest) != 8080 && (ntohs(tcp_header->dest) != 8081)) {
+                continue;
             }
+            
+            printf("Source Checksum: %02X\n", ntohs(tcp_header->check));
+            compute_tcp_checksum(ip_header, (unsigned short*)tcp_header);
+            printf("Computed Checksum: %02X\n", ntohs(tcp_header->check));
         }
     }
 
